1e5e71efa5dbf93223cd09ce72bc36b097f786a2
Lana <tiushkievich00@mail.ru>
2023-10-18 15:53:48 +0200

    feat: add search input

--- src/App.js
+++ src/App.js
@@ -21,7 +21,14 @@ function App() {
         </ul>
       </header>
       <div className="content p-40 ">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
         <div className="d-flex">
           <div className="card">
             <img
