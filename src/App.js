e251d47518d1846f84dd7f305fc568937a5c3865
Lana <tiushkievich00@mail.ru>
2023-10-19 20:18:16 +0200

    feat: add shopping basket

--- src/App.js
+++ src/App.js
@@ -1,6 +1,66 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30 d-flex justify-between">
+            Корзина
+            <img className="cu-p" src="/img/btn-remove.svg" alt="remove" />
+          </h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/sneak1.png)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/sneak1.png)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul className="cartTotalBlock">
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498$</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074$</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.svg" alt="logo" />
